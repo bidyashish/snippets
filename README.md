05523e74d60a7681e939782a1423e675ef3d8ce0
Bidyashish Kumar <bkumar@ultradata.com.au>
2019-12-19 13:36:08 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# snippets
+# Snippets    Javscript
 A short and tiny lists of code and snippets use full in day to day activity and development process
+
+Please feel free to add any snippets 
+
+
+
+
+
+
+#js  #linux #windows  #win10 #git #docs #bash #curl #docker #php #nodejs
+ 
+
+Regards,
+Bidyashish Kumar
+https://github.com/bidyashish/
---
+++ youtube-non-stop.js
@@ -0,0 +1,28 @@
+/* 
+
+A code to  play non stop youtube videos on web browser 
+
+As  Youtube.com has policy to ask "Video paused. Continue watching?" and Stop any continous video being played at an average of 90 minutes.
+
+This code is setup interval to play non-stop videos in Youtube.com
+
+1. Open console (Ctrl + shift + I) -> go to console tab (esc press)
+
+SetInterval has 10 milliseconds time interval
+
+paste the below code in console
+
+
+Bidyashish Kumar
+https://github.com/bidyashish/
+*/
+
+setInterval(function () {
+    if (document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer').length >= 1) {
+        for (let i = 0; i < document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer').length; i++) {
+            if (document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer')[i].innerText == "Video paused. Continue watching?") {
+                document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer')[i].parentNode.parentNode.parentNode.querySelector('#confirm-button').click()
+            }
+        }
+    }
+}, 10)();
